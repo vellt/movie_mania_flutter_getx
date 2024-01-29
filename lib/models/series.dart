305caf7c984f041586fc7027b126485d2e64102e
Szanto Benjamin <szantobenjamin97@gmail.com>
2024-01-29 13:06:12 +0100

    some changes

---
+++ lib/models/series.dart
@@ -0,0 +1,37 @@
+class Series {
+  int id;
+  String name;
+  int season;
+  String image;
+  double rating;
+
+  Series({
+    required this.id,
+    required this.name,
+    required this.season,
+    required this.image,
+    required this.rating,
+  });
+
+  // ha kívülről json adat érkezik azt képes legyen fogadni
+  factory Series.fromJson(Map<String, dynamic> json) {
+    return Series(
+      id: json['id'] as int,
+      name: json['name'] as String,
+      season: json['season'] as int,
+      image: json['image'] as String,
+      rating: json['rating'] as double,
+    );
+  }
+
+  // kifelé tudjunk JSON-t küldeni az adatainkból
+  Map<String, dynamic> toJson() {
+    return {
+      "id": id,
+      "name": name,
+      "season": season,
+      "image": image,
+      "rating": rating,
+    };
+  }
+}
