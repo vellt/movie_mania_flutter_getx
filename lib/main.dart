9ac6af80a6292b43855c7c943cad997d7a568cac
Szanto Benjamin <szantobenjamin97@gmail.com>
2024-02-21 08:34:04 +0100

    getstorage init

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 import 'package:movie_mania/views/first/first_view.dart';
 
-void main() {
+void main() async {
+  await GetStorage.init();
   runApp(GetMaterialApp(
     debugShowCheckedModeBanner: false,
     theme: ThemeData.dark(),
